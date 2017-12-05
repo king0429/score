f48295aa1e516ddbe73c3f18bad37904f24fc35a
void_ <452956364@qq.com>
2017-12-05 09:24:59 +0800

    1205

---
+++ pages/index/index.js
@@ -0,0 +1,25 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+
+  },
+  onLoad: function () {
+    console.log(getApp().globalData.userInfo)
+    var that = this;
+    if(getApp().globalData.userInfo){
+      that.setData({
+        userheader: getApp().globalData.userInfo.avatarUrl,
+        nickname: getApp().globalData.userInfo.nickName
+      })
+    }
+    setTimeout(function(){
+      wx.redirectTo({
+        url: '/pages/signup/signup',
+      })
+    },5000)
+
+  },
+})
